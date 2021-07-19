abef1d7e7c238783b31245a6dbe8261908f49209
William <67127399+thunder-red-star@users.noreply.github.com>
2021-07-19 13:20:03 +0000

    Initial commit

---
+++ src/rest/RESTManager.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const APIRequest = require('./APIRequest');
+const routeBuilder = require('./APIRouter');
+const RequestHandler = require('./RequestHandler');
+const { Error } = require('../errors');
+const Collection = require('../util/Collection');
+const { Endpoints } = require('../util/Constants');
+
+class RESTManager {
+  constructor(client, tokenPrefix = 'Bot') {
+    this.client = client;
+    this.handlers = new Collection();
+    this.tokenPrefix = tokenPrefix;
+    this.versioned = true;
+    this.globalTimeout = null;
+    if (client.options.restSweepInterval > 0) {
+      client.setInterval(() => {
+        this.handlers.sweep(handler => handler._inactive);
+      }, client.options.restSweepInterval * 1000);
+    }
+  }
+
+  get api() {
+    return routeBuilder(this);
+  }
+
+  getAuth() {
+    if (this.client.token && this.client.user && this.client.user.bot) {
+      return `Bot ${this.client.token}`;
+    } else if (this.client.token) {
+      return this.client.token;
+    }
+    throw new Error('TOKEN_MISSING');
+  }
+
+  get cdn() {
+    return Endpoints.CDN(this.client.options.http.cdn);
+  }
+
+  request(method, url, options = {}) {
+    const apiRequest = new APIRequest(this, method, url, options);
+    let handler = this.handlers.get(apiRequest.route);
+
+    if (!handler) {
+      handler = new RequestHandler(this);
+      this.handlers.set(apiRequest.route, handler);
+    }
+
+    return handler.push(apiRequest);
+  }
+
+  get endpoint() {
+    return this.client.options.http.api;
+  }
+
+  set endpoint(endpoint) {
+    this.client.options.http.api = endpoint;
+  }
+}
+
+module.exports = RESTManager;
